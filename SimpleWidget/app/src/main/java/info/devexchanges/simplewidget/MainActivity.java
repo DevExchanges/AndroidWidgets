3a745e17e838c69406d960b902ea9a894bb9c07e
DevExchanges <wandered.vip.007@gmail.com>
2017-01-23 16:37:00 +0700

    add simple widget project

---
+++ SimpleWidget/app/src/main/java/info/devexchanges/simplewidget/MainActivity.java
@@ -0,0 +1,14 @@
+package info.devexchanges.simplewidget;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ SimpleWidget/app/src/main/java/info/devexchanges/simplewidget/SimpleWidget.java
@@ -0,0 +1,34 @@
+package info.devexchanges.simplewidget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.widget.RemoteViews;
+
+public class SimpleWidget extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    private void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                 int appWidgetId) {
+
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_simple);
+        // Construct an Intent object includes web adresss.
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.devexchanges.info/"));
+        // In widget we are not allowing to use intents as usually. We have to use PendingIntent instead of 'startActivity'
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+        // Here the basic operations the remote view can do.
+        views.setOnClickPendingIntent(R.id.tvWidget, pendingIntent);
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+}
