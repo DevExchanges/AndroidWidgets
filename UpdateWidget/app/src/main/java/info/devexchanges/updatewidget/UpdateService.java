4b795356743877ff28c38756c19f4ec304c626a2
DevExchanges <wandered.vip.007@gmail.com>
2017-02-10 17:34:49 +0700

    add updating widget

---
+++ UpdateWidget/app/src/main/java/info/devexchanges/updatewidget/UpdateService.java
@@ -0,0 +1,42 @@
+package info.devexchanges.updatewidget;
+
+import android.app.Service;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.widget.RemoteViews;
+
+import java.util.Calendar;
+
+public class UpdateService extends Service {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        String time = getCurrentDateTime();
+
+        RemoteViews view = new RemoteViews(getPackageName(), R.layout.widget_updating);
+        view.setTextViewText(R.id.txt_widget, time);
+        ComponentName theWidget = new ComponentName(this, UpdatingWidget.class);
+        AppWidgetManager manager = AppWidgetManager.getInstance(this);
+        manager.updateAppWidget(theWidget, view);
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    private String getCurrentDateTime() {
+        Calendar c = Calendar.getInstance();
+        int minute = c.get(Calendar.MINUTE);
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+
+        return hour + ":" + minute;
+    }
+}
+
---
+++ UpdateWidget/app/src/main/java/info/devexchanges/updatewidget/UpdatingWidget.java
@@ -0,0 +1,37 @@
+package info.devexchanges.updatewidget;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.util.Log;
+
+public class UpdatingWidget extends AppWidgetProvider {
+    private PendingIntent pendingIntent;
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        final AlarmManager manager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        final Intent i = new Intent(context, UpdateService.class);
+
+        if (pendingIntent == null) {
+            pendingIntent = PendingIntent.getService(context, 0, i, PendingIntent.FLAG_CANCEL_CURRENT);
+        }
+        manager.setRepeating(AlarmManager.ELAPSED_REALTIME, SystemClock.elapsedRealtime(), 60000, pendingIntent);
+        //if you need to call your pendingIntent less then 60 sec
+        //answer is here:
+        //http://stackoverflow.com/questions/29998313/how-to-run-background-service-after-every-5-sec-not-working-in-android-5-1
+        Log.d("UpdatingWidget: ","onUpdate");
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        Log.d("UpdatingWidget: ","onReceive");
+
+    }
+}
